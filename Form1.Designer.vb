2a0a3e53cae426b1c814dcd14cc678a8f0bfe59b
MDTAWHID-2809 <mridu.robi@gmail.com>
2020-06-16 23:48:47 -0400

    RiskRollGame

---
+++ Form1.Designer.vb
@@ -0,0 +1,294 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.Button8 = New System.Windows.Forms.Button()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(144, 117)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(61, 26)
+        Me.TextBox1.TabIndex = 0
+        Me.TextBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(144, 177)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(61, 26)
+        Me.TextBox2.TabIndex = 1
+        Me.TextBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(211, 100)
+        Me.TextBox3.Multiline = True
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(547, 123)
+        Me.TextBox3.TabIndex = 2
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.Blue
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(211, 102)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(62, 55)
+        Me.Button1.TabIndex = 3
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'Button2
+        '
+        Me.Button2.BackColor = System.Drawing.Color.Blue
+        Me.Button2.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.Location = New System.Drawing.Point(279, 103)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(62, 55)
+        Me.Button2.TabIndex = 4
+        Me.Button2.UseVisualStyleBackColor = False
+        '
+        'Button3
+        '
+        Me.Button3.BackColor = System.Drawing.Color.Blue
+        Me.Button3.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button3.Location = New System.Drawing.Point(347, 100)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(62, 55)
+        Me.Button3.TabIndex = 5
+        Me.Button3.UseVisualStyleBackColor = False
+        '
+        'Button4
+        '
+        Me.Button4.BackColor = System.Drawing.Color.Blue
+        Me.Button4.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button4.Location = New System.Drawing.Point(211, 164)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(62, 55)
+        Me.Button4.TabIndex = 6
+        Me.Button4.UseVisualStyleBackColor = False
+        '
+        'Button5
+        '
+        Me.Button5.BackColor = System.Drawing.Color.Blue
+        Me.Button5.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button5.Location = New System.Drawing.Point(279, 163)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(62, 55)
+        Me.Button5.TabIndex = 7
+        Me.Button5.UseVisualStyleBackColor = False
+        '
+        'Button6
+        '
+        Me.Button6.BackColor = System.Drawing.Color.Blue
+        Me.Button6.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button6.Location = New System.Drawing.Point(347, 161)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(62, 55)
+        Me.Button6.TabIndex = 8
+        Me.Button6.UseVisualStyleBackColor = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
+        Me.Label1.Location = New System.Drawing.Point(452, 120)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(0, 29)
+        Me.Label1.TabIndex = 9
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
+        Me.Label2.Location = New System.Drawing.Point(556, 120)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(0, 29)
+        Me.Label2.TabIndex = 10
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
+        Me.Label3.Location = New System.Drawing.Point(662, 120)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(0, 29)
+        Me.Label3.TabIndex = 11
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
+        Me.Label4.Location = New System.Drawing.Point(452, 174)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(0, 29)
+        Me.Label4.TabIndex = 12
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
+        Me.Label5.Location = New System.Drawing.Point(556, 174)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(0, 29)
+        Me.Label5.TabIndex = 13
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
+        Me.Label6.Location = New System.Drawing.Point(662, 174)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(0, 29)
+        Me.Label6.TabIndex = 14
+        '
+        'Button7
+        '
+        Me.Button7.BackColor = System.Drawing.Color.Gray
+        Me.Button7.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button7.Location = New System.Drawing.Point(440, 249)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(75, 63)
+        Me.Button7.TabIndex = 15
+        Me.Button7.Text = "Roll"
+        Me.Button7.UseVisualStyleBackColor = False
+        '
+        'Button8
+        '
+        Me.Button8.BackColor = System.Drawing.Color.White
+        Me.Button8.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button8.Location = New System.Drawing.Point(240, 259)
+        Me.Button8.Name = "Button8"
+        Me.Button8.Size = New System.Drawing.Size(146, 42)
+        Me.Button8.TabIndex = 16
+        Me.Button8.Text = "Clear"
+        Me.Button8.UseVisualStyleBackColor = False
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.BackColor = System.Drawing.Color.Yellow
+        Me.Label7.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!)
+        Me.Label7.Location = New System.Drawing.Point(33, 112)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(105, 32)
+        Me.Label7.TabIndex = 17
+        Me.Label7.Text = "Attaker"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.BackColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.Label8.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!)
+        Me.Label8.Location = New System.Drawing.Point(6, 174)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(132, 32)
+        Me.Label8.TabIndex = 18
+        Me.Label8.Text = "Defender"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.Label9.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!)
+        Me.Label9.ForeColor = System.Drawing.Color.Yellow
+        Me.Label9.Location = New System.Drawing.Point(373, 34)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(129, 32)
+        Me.Label9.TabIndex = 19
+        Me.Label9.Text = "Who Win"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.DarkCyan
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Button8)
+        Me.Controls.Add(Me.Button7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Button7 As Button
+    Friend WithEvents Button8 As Button
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+End Class
---
+++ Form1.vb
@@ -0,0 +1,210 @@
+﻿Public Class Form1
+    Public attack As Integer
+    Public defender As Integer
+    Public tottalbutton = 3
+    Public remainattack As Integer
+    Public remaindefender As Integer
+    Public arrayattack() As Integer
+    Public arraydefender() As Integer
+    Public a As Integer
+    Public b As Integer
+    Public c As Integer
+    Public d As Integer
+    Public l As Integer
+    Public f As Integer
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        attack = Convert.ToInt32(TextBox1.Text)
+        defender = Convert.ToInt32(TextBox2.Text)
+        crlr()
+
+        If defender <> 0 Then
+            If attack >= tottalbutton Then
+                createrandom()
+                Button1.Text = CStr(a)
+                Button2.Text = CStr(b)
+                Button3.Text = CStr(c)
+
+            ElseIf attack = 2 Then
+
+
+                createrandom()
+                Button1.Text = CStr(a)
+                Button2.Text = CStr(b)
+                Button3.Text = "0"
+
+            ElseIf attack = 1 Then
+                createrandom()
+                Button1.Text = CStr(a)
+                Button2.Text = "0"
+                Button3.Text = "0"
+            ElseIf attack = 0 Then
+                Button1.Text = "0"
+                Button2.Text = "0"
+                Button3.Text = "0"
+            End If
+        End If
+        If attack <> 0 Then
+            If defender >= tottalbutton Then
+
+                createrandom()
+                Button4.Text = CStr(d)
+                Button5.Text = CStr(l)
+                Button6.Text = CStr(f)
+            ElseIf defender = 2 Then
+                createrandom()
+                Button4.Text = CStr(d)
+                Button5.Text = CStr(l)
+                Button6.Text = "0"
+            ElseIf defender = 1 Then
+                createrandom()
+
+                Button4.Text = CStr(d)
+                Button5.Text = "0"
+                Button6.Text = "0"
+            ElseIf defender = 0 Then
+                Button4.Text = "0"
+                Button5.Text = "0"
+                Button6.Text = "0"
+
+            End If
+        End If
+
+        If (Convert.ToInt32(Button1.Text) <> 0 And Convert.ToInt32(Button4.Text) <> 0) Then
+            If Convert.ToInt32(Button1.Text) > Convert.ToInt32(Button4.Text) Then
+                defender = defender - 1
+                Label4.Text = "Lost"
+                Label1.Text = "Win"
+            Else
+                attack = attack - 1
+                Label1.Text = "Lost"
+                Label4.Text = "Win"
+            End If
+        End If
+        If (Convert.ToInt32(Button2.Text) <> 0 And Convert.ToInt32(Button5.Text) <> 0) Then
+            If Convert.ToInt32(Button2.Text) > Convert.ToInt32(Button5.Text) Then
+                defender = defender - 1
+                Label5.Text = "Lost"
+                Label2.Text = "Win"
+            Else
+                attack = attack - 1
+                Label2.Text = "Lost"
+                Label5.Text = "win"
+            End If
+        End If
+        If (Convert.ToInt32(Button3.Text) <> 0 And Convert.ToInt32(Button6.Text) <> 0) Then
+            If Convert.ToInt32(Button3.Text) > Convert.ToInt32(Button6.Text) Then
+                defender = defender - 1
+                Label6.Text = "Lost"
+                Label3.Text = "Win"
+            Else
+                attack = attack - 1
+                Label3.Text = "Lost"
+                Label6.Text = "Win"
+            End If
+        End If
+        TextBox1.Text = CStr(attack)
+        TextBox2.Text = CStr(defender)
+        changecolr()
+        win()
+    End Sub
+
+    Public Sub createrandom()
+
+        a = Int((6 - 1 + 1) * Rnd() + 1)
+        b = Int((6 - 1 + 1) * Rnd() + 1)
+        c = Int((6 - 1 + 1) * Rnd() + 1)
+        d = Int((6 - 1 + 1) * Rnd() + 1)
+        l = Int((6 - 1 + 1) * Rnd() + 1)
+        f = Int((6 - 1 + 1) * Rnd() + 1)
+        Dim a1 = New Integer() {a, b, c}
+        Dim a2 = New Integer() {d, l, f}
+        Array.Sort(a1)
+        a = a1(2)
+        b = a1(1)
+        c = a1(0)
+
+        Array.Sort(a2)
+        d = a2(2)
+        l = a2(1)
+        f = a2(0)
+    End Sub
+
+    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
+        Button1.Text = " "
+        Button2.Text = " "
+        Button3.Text = " "
+        Button4.Text = " "
+        Button5.Text = " "
+        Button6.Text = " "
+        Label1.Text = " "
+        Label2.Text = " "
+        Label4.Text = " "
+        Label5.Text = " "
+        Label3.Text = " "
+        Label6.Text = " "
+        TextBox1.Text = " "
+        TextBox2.Text = " "
+        Button1.BackColor = Color.Blue
+        Button2.BackColor = Color.Blue
+        Button3.BackColor = Color.Blue
+        Button4.BackColor = Color.Blue
+        Button5.BackColor = Color.Blue
+        Button6.BackColor = Color.Blue
+
+
+    End Sub
+    Public Sub crlr()
+        Label1.Text = " "
+        Label2.Text = " "
+        Label4.Text = " "
+        Label5.Text = " "
+        Label3.Text = " "
+        Label6.Text = " "
+        TextBox1.Text = " "
+        TextBox2.Text = " "
+        Label9.Text = "Who Win"
+
+    End Sub
+    Public Sub changecolr()
+        If Convert.ToInt32(Button1.Text) = 0 Then
+            Button1.BackColor = Color.Red
+        Else
+            Button1.BackColor = Color.Blue
+        End If
+        If Convert.ToInt32(Button2.Text) = 0 Then
+            Button2.BackColor = Color.Red
+        Else
+            Button2.BackColor = Color.Blue
+        End If
+        If Convert.ToInt32(Button3.Text) = 0 Then
+            Button3.BackColor = Color.Red
+        Else
+            Button3.BackColor = Color.Blue
+        End If
+        If Convert.ToInt32(Button4.Text) = 0 Then
+            Button4.BackColor = Color.Red
+        Else
+            Button4.BackColor = Color.Blue
+        End If
+        If Convert.ToInt32(Button5.Text) = 0 Then
+            Button5.BackColor = Color.Red
+        Else
+            Button5.BackColor = Color.Blue
+        End If
+        If Convert.ToInt32(Button6.Text) = 0 Then
+            Button6.BackColor = Color.Red
+        Else
+            Button6.BackColor = Color.Blue
+        End If
+    End Sub
+    Public Sub win()
+        If (Convert.ToInt32(Button1.Text) = 0 And Convert.ToInt32(Button2.Text) = 0 And Convert.ToInt32(Button3.Text) = 0) Then
+            Label9.Text = "Defender Win"
+        End If
+        If (Convert.ToInt32(Button4.Text) = 0 And Convert.ToInt32(Button5.Text) = 0 And Convert.ToInt32(Button6.Text) = 0) Then
+            Label9.Text = "Attacker Win"
+        End If
+    End Sub
+
+
+End Class
